bac84f327eeceb30b11223faddb6dbb996a0e3d7
VicenBayugar <vicentebayugar@gmail.com>
2022-03-28 12:16:01 -0300

    Navbar

---
+++ client/NoCountryCinema/src/components/Navbar/Navbarr.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Navbarr = () => {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home" className="fs-3">
+          NC Cinema
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto fs-5">
+            <Nav.Link href="#promociones">Promociones</Nav.Link>
+            <Nav.Link href="#candy">Candy</Nav.Link>
+            <NavDropdown title="Películas" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">
+                Categoría 1
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Categoría 2
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+                Categoría 3
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Todas las categorías
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Navbarr;
