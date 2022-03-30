ed4f61f50fa392b96909d37378cb98a8f382a4f4
CM-Gimenez <75501546+CM-Gimenez@users.noreply.github.com>
2022-03-30 19:09:26 -0300

    agregué footer

--- front-end/NoCountryCinema/src/App.jsx
+++ front-end/NoCountryCinema/src/App.jsx
@@ -1,12 +1,14 @@
 import MoviesContainer from './components/MoviesSection/MoviesContainer'
 import "./App.css";
 import Header from "./components/Header/Header";
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <MoviesContainer/>
+      <Footer/>
     </div>
   );
 }
