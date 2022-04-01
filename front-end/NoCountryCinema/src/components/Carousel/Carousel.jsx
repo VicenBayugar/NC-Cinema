43cf45ffae678269d75e6418b3daead0196d7a49
kiddopro <ms.prgramador@gmail.com>
2022-04-01 10:27:47 -0300

    modified carousel

--- front-end/NoCountryCinema/src/components/Carousel/Carousel.jsx
+++ front-end/NoCountryCinema/src/components/Carousel/Carousel.jsx
@@ -12,34 +12,34 @@ export const Carro = () => {
       src="https://static.cinemarkhoyts.com.ar/Images/Highlights/437.png"
       alt="First slide"
     />
-    <Carousel.Caption>
+    {/* <Carousel.Caption>
       <h3>First slide label</h3>
       <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
+    </Carousel.Caption> */}
   </Carousel.Item>
   <Carousel.Item>
     <img
-      className="d-block w-100"
+      className="d-block w-100 img-test"
       src="https://static.cinemarkhoyts.com.ar/Images/Highlights/431.png"
       alt="Second slide"
     />
 
-    <Carousel.Caption>
+    {/* <Carousel.Caption>
       <h3>Second slide label</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
+    </Carousel.Caption> */}
   </Carousel.Item>
   <Carousel.Item>
     <img
-      className="d-block w-100"
+      className="d-block w-100 img-test"
       src="https://static.cinemarkhoyts.com.ar/Images/Highlights/434.png"
       alt="Third slide"
     />
 
-    <Carousel.Caption>
+    {/* <Carousel.Caption>
       <h3>Third slide label</h3>
       <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
+    </Carousel.Caption> */}
   </Carousel.Item>
       </Carousel>
       </Container>
