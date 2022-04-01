56acd1e887de1cc866aa0997e7ba97d63ed4a0d0
kiddopro <ms.prgramador@gmail.com>
2022-04-01 19:46:33 -0300

    added font-awesome and react0icons libraries for icons

--- front-end/NoCountryCinema/src/components/DropdownBoxes/DropdownSection.jsx
+++ front-end/NoCountryCinema/src/components/DropdownBoxes/DropdownSection.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
-import { Container } from 'react-bootstrap';
+import React from 'react';
+// import { AiOutlineShoppingCart } from 'react-icons/all';
+import { DropdownButton, Container, Dropdown, Button } from 'react-bootstrap';
 export const DropdownSection = () => {
   return (
-    <Container>DropdownSection</Container>
-  )
-}
+    <Container className="my-3">
+      <h5>COMPRA TUS ENTRADAS ONLINE</h5>
+      <Container className="d-flex">
+        <DropdownButton
+          className="mx-2"
+          variant="light"
+          title="FECHA"
+          id="input-group-dropdown-1">
+          <Dropdown.Item href="#">Action</Dropdown.Item>
+          <Dropdown.Item href="#">Another action</Dropdown.Item>
+          <Dropdown.Item href="#">Something else here</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item href="#">Separated link</Dropdown.Item>
+        </DropdownButton>
+        <DropdownButton
+          className="mx-2"
+          variant="light"
+          title="PELÍCULA"
+          id="input-group-dropdown-1">
+          <Dropdown.Item href="#">Action</Dropdown.Item>
+          <Dropdown.Item href="#">Another action</Dropdown.Item>
+          <Dropdown.Item href="#">Something else here</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item href="#">Separated link</Dropdown.Item>
+        </DropdownButton>
+        <DropdownButton
+          className="mx-2"
+          variant="light"
+          title="HORA"
+          id="input-group-dropdown-1">
+          <Dropdown.Item href="#">Action</Dropdown.Item>
+          <Dropdown.Item href="#">Another action</Dropdown.Item>
+          <Dropdown.Item href="#">Something else here</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item href="#">Separated link</Dropdown.Item>
+        </DropdownButton>
+        <Button
+          className="mx-2"
+          style={{
+            backgroundColor: '#00C5BC',
+            border: 'none',
+            fontWeight: 'bolder',
+          }}>
+          <i className="fa-solid fa-cart-shopping"></i>
+          &nbsp; COMPRAR
+        </Button>
+      </Container>
+    </Container>
+  );
+};
