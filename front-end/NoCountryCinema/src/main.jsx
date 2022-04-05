da5692baf91cd9ec2902d9de7b30a991ccc92c2f
kiddopro <ms.prgramador@gmail.com>
2022-04-04 22:17:55 -0300

    merged with register branch

--- front-end/NoCountryCinema/src/main.jsx
+++ front-end/NoCountryCinema/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Login } from './components/Login/Login';
+import { Login } from './pages/LoginPage/Login';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 
