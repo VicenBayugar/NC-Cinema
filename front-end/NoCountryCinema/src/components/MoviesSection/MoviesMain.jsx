81573ed63a66bce4df64ef7fe514b2161267c7b2
VicenBayugar <vicentebayugar@gmail.com>
2022-04-05 18:01:27 -0300

    Agrego axios y spinner

--- front-end/NoCountryCinema/src/components/MoviesSection/MoviesMain.jsx
+++ front-end/NoCountryCinema/src/components/MoviesSection/MoviesMain.jsx
@@ -1,35 +1,60 @@
-import { Container, CardGroup, Row } from 'react-bootstrap';
+import { Container, CardGroup, Row, Spinner } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Movie from './Movie';
 import React, { useEffect, useState } from 'react';
-import Data from '../../data/movies.json';
+import axios from 'axios';
 
 const MoviesMain = () => {
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState()
 
   useEffect(() => {
+    setLoading(true);
     const obtenerMovies = async () => {
-      const data = await fetch('http://localhost:3005/api/movies');
-      const moviesObtenidas = await data.json();
-      const moviesFinal = moviesObtenidas.response.filter(movie => movie.premiere == false)
-      setMovies(moviesFinal);
+      try {
+        const { data } = await axios.get('http://localhost:3005/api/movies');
+        const moviesFinal = data.response.filter(
+          movie => movie.premiere == false,
+        );
+        setMovies(moviesFinal);
+        setLoading(false);
+      } catch (err) {
+        // setError(err.message)
+        console.log(err.message);
+      } 
     };
     obtenerMovies();
   }, []);
 
   return (
     <Container className="mt-5 align-items-center">
-      <h3>PELÍCULAS</h3>
-      <hr />
-      <CardGroup className='mt-2 '>
-        <Row xs={1} md={2} lg={6} className="g-4 justify-content-center">
-          {movies.map(item => {
-            return (
-              <Movie key={item._id} name={item.title} imgUrl={item.image} />
-            );
-          })}
-        </Row>
-      </CardGroup>
+      {loading ? (
+        <Container
+          className="d-flex justify-content-center"
+          style={{ height: '100rem' }}>
+          <Spinner
+            animation="border"
+            role="status"
+            style={{ width: '7rem', height: '7rem' }}>
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </Container>
+      ) : (
+        <>
+          <h3>PELÍCULAS</h3>
+          <hr />
+          <CardGroup className="mt-2 ">
+            <Row xs={1} md={2} lg={6} className="g-4 justify-content-center">
+              {movies.map(item => {
+                return (
+                  <Movie key={item._id} name={item.title} imgUrl={item.image} />
+                );
+              })}
+            </Row>
+          </CardGroup>
+        </>
+      )}
     </Container>
   );
 };
