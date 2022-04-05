b526bd9f5a719806e4deb4db04047558fef8693c
VicenBayugar <vicentebayugar@gmail.com>
2022-04-05 13:00:00 -0300

    Llamo a la api de movies

--- front-end/NoCountryCinema/src/components/MoviesSection/Movie.jsx
+++ front-end/NoCountryCinema/src/components/MoviesSection/Movie.jsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Movie = (props) => {
   return (
     <Card bg='dark' border='secondary' className='p-2' style={{ width: '20rem', height: '35rem' }}>
-      <Card.Img variant="top" className='img-fluid h-100' style={{objectFit: 'contain'}} src={props.imgUrl} /> 
+      <Card.Img variant="top" className='img-fluid w-100 h-100' style={{objectFit: 'cover'}} src={props.imgUrl} /> 
       <Card.Body className='w-100'>
         <Card.Title className='text-wrap text-center'>{props.name}</Card.Title>
       </Card.Body>
