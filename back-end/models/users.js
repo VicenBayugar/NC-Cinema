68799c7490ceeb0b5975d766174c902d403bf0f3
nicolagardo <nicolagardo@gmail.com>
2022-04-07 10:32:48 -0300

    user model creado

---
+++ back-end/models/users.js
@@ -0,0 +1,45 @@
+const monogoose = require("mongoose")
+
+const UserSchema = new monogoose.Schema(
+    {
+       
+        name:{
+            type:String
+        },
+        apellido:{
+            type:String
+        },
+        email:{
+            type:String,
+            type: unique    
+        },
+        password:{
+            type:String
+        },
+        documento:{
+            type:String,
+            unique: true
+        },
+        typeDoc:{
+            type:String
+        },
+        role: {
+            type: ["user", "admin"],
+            default: "user"
+
+        },
+        peliculasCompradas:{
+            type:Array
+        },
+        butacasReservadas:{
+            type:Array
+        }
+
+        
+    },
+    {
+        timestamps: true, //TODO: crea automaticamente, los campos createdAT, updateAt
+        versionKey: false
+    }
+);
+module.exports = monogoose.model("users", UserSchema)
