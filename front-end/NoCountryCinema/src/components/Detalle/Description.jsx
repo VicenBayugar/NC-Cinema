935db6f4b916e1007f8d87581982e2da417d3612
CM-Gimenez <75501546+CM-Gimenez@users.noreply.github.com>
2022-04-12 09:06:56 -0300

    Profile (ruta privada)

--- front-end/NoCountryCinema/src/components/Detalle/Description.jsx
+++ front-end/NoCountryCinema/src/components/Detalle/Description.jsx
@@ -1,95 +1,96 @@
 import React from 'react';
-import { Col, Container, Row, Table } from 'react-bootstrap';
+import { Button, Col, Container, Row, Table } from 'react-bootstrap';
 import axios from 'axios';
 import './detail.css';
 import ReactPlayer from 'react-player';
+import { useNavigate } from 'react-router-dom';
 
 const Description = ({ movie }) => {
   return (
     <>
 
-    {movie && (
-    <Container>
-       <Row className='contenedor'>
-        <ReactPlayer
-          url={movie.trailer}
-          width='100%'
-          height='100%'
-          controls
-          className='player' />
-      </Row>
-      <Row className='row-tittle'>
-        <h2>{movie.title} </h2>
-      </Row>
-      <Row>
-        <Col xl={6} className='d-flex justify-content-center pb-4 pt-4'>
-          <img src={movie.image} className='img-fluid' alt="imagenPeli" />
-        </Col >
-        <Col xl={6}>
-          <Row>
-            <h3>Sinopsis</h3>
-            <p>{movie.resume}</p>
+      {movie && (
+        <Container>
+          <Row className='contenedor'>
+            <ReactPlayer
+              url={movie.trailer}
+              width='100%'
+              height='100%'
+              controls
+              className='player' />
+          </Row>
+          <Row className='row-tittle'>
+            <h2>{movie.title} </h2>
           </Row>
           <Row>
-            <Table striped hover variant="dark">
-              <thead>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Director: </td>
-                  <td>{movie.director}</td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>Origen: </td>
-                  <td>{movie.origin}</td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>Reparto: </td>
-                  <td>{movie.actors}</td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>Género: </td>
-                  <td>{movie.genre}</td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>Duracion: </td>
-                  <td>{movie.length} Minutos</td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td >Idiomas: </td>
-                  <td className='col-5'>{movie.language.map((mov, idx)=>{
-                    return(
-                      <p id='p' key={idx}>{mov}</p>
-                    )
-                  })}</td>
-                  <td className='col-5'>{movie.tags.map((mov, idx)=>{
-                    return(
-                      <p id='p' key={idx}>{mov}</p>
-                    )
-                  })}</td>
-                </tr>
-                <tr>
-                  <td>Disponible: </td>
-                  <td id='disponible'>2D</td>
-                  <td id='disponible'>3D</td>
-                </tr>
+            <Col xl={6} className='d-flex justify-content-center pb-4 pt-4'>
+              <img src={movie.image} className='img-fluid' alt="imagenPeli" />
+            </Col >
+            <Col xl={6}>
+              <Row>
+                <h3>Sinopsis</h3>
+                <p>{movie.resume}</p>
+              </Row>
+              <Row>
+                <Table striped hover variant="dark">
+                  <thead>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Director: </td>
+                      <td>{movie.director}</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>Origen: </td>
+                      <td>{movie.origin}</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>Reparto: </td>
+                      <td>{movie.actors}</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>Género: </td>
+                      <td>{movie.genre}</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>Duracion: </td>
+                      <td>{movie.length} Minutos</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td >Idiomas: </td>
+                      <td className='col-5'>{movie.language.map((mov, idx) => {
+                        return (
+                          <p id='p' key={idx}>{mov}</p>
+                        )
+                      })}</td>
+                      <td className='col-5'>{movie.tags.map((mov, idx) => {
+                        return (
+                          <p id='p' key={idx}>{mov}</p>
+                        )
+                      })}</td>
+                    </tr>
+                    <tr>
+                      <td>Disponible: </td>
+                      <td id='disponible'>2D</td>
+                      <td id='disponible'>3D</td>
+                    </tr>
 
 
-              </tbody>
-            </Table>
+                  </tbody>
+                </Table>
+              </Row>
+            </Col>
           </Row>
-        </Col>
-      </Row>
-      <Row>
-
-      </Row>
-    </Container>
-    )}
+          <Container className='text-center mb-2'>
+            <Button onClick={handlerClick}>Comprar</Button>
+          </Container>
+        </Container>
+      )}
 
     </>
   );
--- front-end/NoCountryCinema/src/pages/Profile/Profile.jsx
+++ front-end/NoCountryCinema/src/pages/Profile/Profile.jsx
@@ -1,83 +1,102 @@
-import React from 'react';
-import { Container, Row, Table, Col, Card, Button } from 'react-bootstrap';
+import React from 'react'
+import { Navigate, useNavigate } from 'react-router-dom'
+import {Button, Container, Row, Table, Col, Card } from 'react-bootstrap';
 
 const Profile = () => {
-  return (
-    <Container>
-      <Row className="row-tittle text-center">
-        <h2 className="mt-5">Perfil</h2>
-      </Row>
-      <Row className="mt-5 mb-5">
-        <Col xl={6}>
-          <Row className="ps-5 pe-5">
-            <h2 className="mb-5 text-center">Información personal</h2>
-            <Table striped hover variant="dark">
-              <tbody>
-                <tr>
-                  <td>Nombre: </td>
-                  <td>Vicente</td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>Apellido: </td>
-                  <td>Bayúgar</td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>Correo electrónico: </td>
-                  <td>vicentebayugar@gmail.com</td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>Tipo de documento: </td>
-                  <td>DNI</td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>N° de documento: </td>
-                  <td>12345678</td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>Método de pago: </td>
-                  <td>VISA crédito</td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>N° de tarjeta: </td>
-                  <td>XXXX XXXX XXXX 1234</td>
-                  <td></td>
-                </tr>
-              </tbody>
-            </Table>
-          </Row>
-        </Col>
-        <Col xl={6} className="pb-4">
-          <Row className="ps-5 pe-5">
-            <h2 className="mb-5 text-center">Mis películas:</h2>
-            <Card className="text-center bg-dark">
-              <Card.Header>Sala 2</Card.Header>
-              <Card.Body>
-                <Card.Title className="mb-4">Batman</Card.Title>
-                <Card.Text>
-                  <i class="bi bi-calendar3 fs-5 pe-1"></i>Hoy, 11 de Abril,
-                  2022
-                </Card.Text>
-                <Card.Text>
-                  <i class="bi bi-stopwatch fs-5 pe-1"></i>6:00 pm
-                </Card.Text>
-                <Card.Text>
-                  <i class="bi bi-geo-alt fs-5 pe-1"></i>Ubicaciones: P-6, P-7
-                </Card.Text>
-                <Button variant="primary">Cambiar ubicación</Button>
-              </Card.Body>
-              <Card.Footer className="text-muted">2D - DOBLADA</Card.Footer>
-            </Card>
-          </Row>
-        </Col>
-      </Row>
-    </Container>
-  );
-};
+  const navigate = useNavigate();
+  function handlerClick(e){
+    e.preventDefault()
+    sessionStorage.clear()
+    navigate('/')
 
-export default Profile;
+  }
+
+  let token = sessionStorage.getItem('token')
+  return (
+      <>
+      {!token && <Navigate to={'/login'}/>}
+      <Container>
+        <Container  className="text-end"><Button style={{
+          color:"whitesmoke"
+        }} size='xs' variant='outline' onClick={handlerClick}>Cerrar Sesión</Button></Container>
+        <Row className="row-tittle text-center">
+          <h2 className="mt-1">Perfil</h2>
+        </Row>
+        <Row className="mt-5 mb-5">
+          <Col xl={6}>
+            <Row className="ps-5 pe-5">
+              <h2 className="mb-5 text-center">Información personal</h2>
+              <Table striped hover variant="dark">
+                <tbody>
+                  <tr>
+                    <td>Nombre: </td>
+                    <td>Vicente</td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>Apellido: </td>
+                    <td>Bayúgar</td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>Correo electrónico: </td>
+                    <td>vicentebayugar@gmail.com</td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>Tipo de documento: </td>
+                    <td>DNI</td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>N° de documento: </td>
+                    <td>12345678</td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>Método de pago: </td>
+                    <td>VISA crédito</td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>N° de tarjeta: </td>
+                    <td>XXXX XXXX XXXX 1234</td>
+                    <td></td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Row>
+          </Col>
+          <Col xl={6} className="pb-4">
+            <Row className="ps-5 pe-5">
+              <h2 className="mb-5 text-center">Mis películas:</h2>
+              <Card className="text-center bg-dark">
+                <Card.Header>Sala 2</Card.Header>
+                <Card.Body>
+                  <Card.Title className="mb-4">Batman</Card.Title>
+                  <Card.Text>
+                    <i className="bi bi-calendar3 fs-5 pe-1"></i>Hoy, 11 de Abril,
+                    2022
+                  </Card.Text>
+                  <Card.Text>
+                    <i className="bi bi-stopwatch fs-5 pe-1"></i>6:00 pm
+                  </Card.Text>
+                  <Card.Text>
+                    <i className="bi bi-geo-alt fs-5 pe-1"></i>Ubicaciones: P-6, P-7
+                  </Card.Text>
+                  <Button variant="primary">Cambiar ubicación</Button>
+                </Card.Body>
+                <Card.Footer className="text-muted">2D - DOBLADA</Card.Footer>
+              </Card>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+      </>
+    );
+  };
+  
+ 
+
+
+export default Profile
