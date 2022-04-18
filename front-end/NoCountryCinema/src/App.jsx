bbe721906883c170fd52fcde7af4ff3c7b2342ef
CM-Gimenez <75501546+CM-Gimenez@users.noreply.github.com>
2022-04-17 23:16:38 -0300

    se crea dashboard de pagina, falta dar privacidad

--- front-end/NoCountryCinema/src/App.jsx
+++ front-end/NoCountryCinema/src/App.jsx
@@ -1,12 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router} from 'react-router-dom'
+//styles
 import "./App.css";
+//Components
 import Header from "./components/Header/Header";
 import Footer from './components/Footer/Footer';
-import Home from './pages/Home';
-import Profile from './pages/Profile/Profile';
-import MovieDetails from './pages/MovieDetail/MovieDetails';
-import RegisterPage from './pages/RegisterPage/RegisterPage';
-import {Login} from './pages/LoginPage/Login'
+import AppRoutes from './Rutas/AppRoutes';
+
 
 
 function App() {
@@ -14,13 +13,7 @@ function App() {
     <div className="App">
       <Router>
         <Header />
-        <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/login' element={<Login/>} />
-          <Route path='/register' element={<RegisterPage />} />
-          <Route path='/detalle/:title' element={<MovieDetails />} />
-          <Route path='/profile' element={<Profile/>}/>
-        </Routes>
+          <AppRoutes/>
         <Footer />
       </Router>
     </div>
---
+++ front-end/NoCountryCinema/src/pages/Dashboard/ModalEdit.jsx
@@ -0,0 +1,62 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'react-bootstrap'
+
+const ModalEdit = ({ modal, setModal, movie_id }) => {
+    //const [movie, setmovie] = useState();
+
+    /*useEffect(async () => {
+        const enpoint = (`http://localhost:3005/api/movies/${movie_id}`)
+        await axios.get(enpoint, movie)
+        .then((res) => {
+            const apidata = res.data;
+            setmovie(apidata);  
+        })  
+        .catch((error) => {
+            console.log(error);
+            sweetAlert({ title: 'ops', icon: 'error' });
+        });
+    }, [])*/ 
+
+
+
+    return (
+        <Modal show={modal}>
+            <ModalHeader style={{ display: 'block' }}>
+                <h4>Editar Pelicula</h4>
+            </ModalHeader>
+            <ModalBody>
+                <div className="form-group">
+                    <label htmlFor="nombre">Nombre</label>
+                    <input className="form-control" type='text' name='name' id='name'></input>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="nombre">Origen</label>
+                    <input className="form-control" type='text' name='origin' id='origin'></input>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="nombre">Director</label>
+                    <input className="form-control" type='text' name='director' id='director'></input>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="nombre">Reparto</label>
+                    <input className="form-control" type='text' name='actors' id='actors'></input>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="nombre">Género</label>
+                    <input className="form-control" type='text' name='genre' id='genre'></input>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="nombre">Calificación</label>
+                    <input className="form-control" type='text' name='qualify' id='qualify'></input>
+                </div>
+            </ModalBody>
+            <ModalFooter>
+                <Button>Guardar cambios</Button>
+                <Button onClick={() => setModal(false)}> Cancelar</Button>
+            </ModalFooter>
+        </Modal>
+    )
+}
+
+export default ModalEdit
