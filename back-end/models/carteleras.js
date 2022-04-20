f70de0629bc3b8bd8c8eb0b34f8861aaa6cffa4e
nicolagardo <nicolagardo@gmail.com>
2022-04-20 10:05:18 -0300

    esquema cartelera

--- back-end/models/carteleras.js
+++ back-end/models/carteleras.js
@@ -2,63 +2,33 @@ const mongoose = require('mongoose');
 //const  Schema  = mongoose.Schema;
 
 const CarteleraSchema = new mongoose.Schema({
-       
-    // fechaDia: [
-    //     {Dia:{ 
-    //         type :Date}}, 
-    //     {horas:{
-    //         type :[Date]}}],
-    // asientos:
-    //         [{salaNombre:{type: Number},
-    //         estado:{
-    //             type:Boolean}}, 
-    //         {numero:{
-    //             type:Number}
-    //         }] 
-    // fechaDia: [
-    //     {Dia:{ 
-    //         type :Date}}, 
-    //     {horas:{
-    //         type :[Date]}},
-    //     {asientos:
-    //         [{salaNombre:{type: Number},
-    //         estado:{
-    //             type:Boolean}}, 
-    //         {numero:{
-    //             type:Number}
-    //         }]} 
-        
-    //     ],
-    // movie: {
-    //     type: mongoose.Schema.Types.ObjectId
-    // },
-    // sala: {
-    //     type: mongoose.Schema.Types.ObjectId
-    // }
+
         salaId: {
-            type: mongoose.Schema.Types.ObjectId
-        },
-        movie: {
-            type: [mongoose.Schema.Types.ObjectId]
-        },
-        diasHorarios:{ 
-        type :Date},
-        horas: [
-            {horas:{
-                type :[Date]}},
-            {asientos:
-                [
-                {salaNombre:{type: String},
-                estado:{
-                    type:Boolean}}, 
-                {numero:{
-                    type:Number}
-                }]} 
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "salas"
+            },
+        movieId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "movies"
+            },
+        datos: [
+                {dia:{
+                    type: String}},
+                {schedule: [
+                            {hora:{
+                                type :String}},
+                            {butacas:[  
+                                        {numero:{
+                                                type:Number}},
+                                        {estado:{
+                                                type:Boolean}} 
+                                        
+                                    ]} 
             
-            ]
-        
+                            ]}
+                
+                ]       
        
-    
     },
     {
         timestamps: true, // TODO: crea automaticamente, los campos createdAT, updateAt
