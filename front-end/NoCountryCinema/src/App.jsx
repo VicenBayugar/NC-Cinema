9a63fe72f003abf913d16af98112430ca3452e02
nicolagardo <nicolagardo@gmail.com>
2022-04-22 09:05:44 -0300

    sarasa

--- front-end/NoCountryCinema/src/App.jsx
+++ front-end/NoCountryCinema/src/App.jsx
@@ -15,19 +15,7 @@ function App() {
     <div className="App">
       <Router>
         <Header />
-<<<<<<< HEAD
-        <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/login' element={<Login/>} />
-          <Route path='/register' element={<RegisterPage />} />
-          <Route path='/detalle/:title' element={<MovieDetails />} />
-          <Route path='/profile' element={<Profile/>}/>
-     
-
-        </Routes>
-=======
           <AppRoutes/>
->>>>>>> d4104d44249be56b15530e1f33cc1d3aa0ed7ee9
         <Footer />
       </Router>
     </div>
